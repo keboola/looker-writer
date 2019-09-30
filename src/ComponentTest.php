3650dd188963e109bb589423ffb84ec8a30a3a66
Tomáš Fejfar <tomas.fejfar@gmail.com>
2019-09-30 16:10:27 +0200

    Generate RUNID in tests from Storage API

--- src/ComponentTest.php
+++ src/ComponentTest.php
@@ -1,15 +1,30 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 namespace Keboola\LookerWriter;
 
 use Keboola\Component\Logger;
+use Keboola\StorageApi\Client;
 use PHPUnit\Framework\TestCase;
 
 class ComponentTest extends TestCase
 {
+    /** @var Client */
+    private $client;
+
+    public function setUp(): void
+    {
+        $config = new Config([]);
+        $this->client = new Client([
+            'token' => $config->getStorageApiToken(),
+            'url' => $config->getStorageApiUrl(),
+        ]);
+    }
 
     public function testWillConnect(): void
     {
+        putenv('KBC_RUNID=' . $this->client->generateRunId());
         $component = new Component(new Logger());
         $component->execute();
     }
