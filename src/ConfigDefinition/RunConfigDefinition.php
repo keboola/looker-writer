60cc97cfa2f61b2855e8aa2882c16bb0ee43dacc
Tomáš Fejfar <tomas.fejfar@gmail.com>
2019-11-01 12:00:42 +0100

    Allow "looker" node in run config

--- src/ConfigDefinition/RunConfigDefinition.php
+++ src/ConfigDefinition/RunConfigDefinition.php
@@ -6,6 +6,7 @@ namespace Keboola\LookerWriter\ConfigDefinition;
 
 use Keboola\Component\Config\BaseConfigDefinition;
 use Keboola\LookerWriter\ConfigDefinition\Node\DbNodeDefinition;
+use Keboola\LookerWriter\ConfigDefinition\Node\LookerNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
 class RunConfigDefinition extends BaseConfigDefinition
@@ -18,6 +19,7 @@ class RunConfigDefinition extends BaseConfigDefinition
         $parametersNode
             ->children()
                 ->append((new DbNodeDefinition('db'))->isRequired())
+                ->append(new LookerNodeDefinition())
                 ->arrayNode('tables')
                     ->requiresAtLeastOneElement()
                     ->isRequired()
