bf3f9050d714c8bf99754b9c14082a775634bf10
Ondra Jodas <ondrej.jodas@keboola.com>
2021-11-04 07:34:08 +0100

    more logging

--- kbctests.sh
+++ kbctests.sh
@@ -6,6 +6,7 @@ do
   export CONFIG="{\"component\": \"${KBC_DEVELOPERPORTAL_APP}\",\"mode\": \"run\",\"config\": \"${config}\",\"tag\": \"${TAG}\"}"
 
   jobid=$(curl -s -X POST -H "X-StorageApi-Token: ${KBC_STORAGE_TOKEN}" -H "Content-Type: application/json" -d "${CONFIG}" https://queue.keboola.com/jobs | jq -r '.id')
+  echo "Job '$jobid' started"
   jobfinished=false
   while ! $jobfinished; do
     sleep 5
@@ -15,8 +16,12 @@ do
 
     if [[ "$jobfinished" == "true" && "$status" != "success" ]]
     then
-      echo "$jobid has been $status"
+      echo "Job '$jobid' ended with status '$status'"
       exit 1
+    fi
+    if [ "$jobfinished" == "true" ]
+    then
+      echo "Job '$jobid' has been successful"
     fi
   done
 done
